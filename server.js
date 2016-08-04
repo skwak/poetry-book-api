d3799697877e95879c67b3e7468a9d8c20767fd6
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2016-08-03 22:58:50 -0700

    add dotenv stuff

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 // require('babel-core').transform('code');
 'use strict';
 
+require('dotenv').config();
+
 const Hapi = require('hapi');
 
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
 
-server.start((err) => {
+server.route({
+  method: 'GET',
+  path: '/api',
+  handler: function(request, reply) {
+    return reply('yayaya');
+  }
+})
 
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
+// http://dev.are.na/oauth/authorize
+//    ?client_id=YOUR_CLIENT_ID
+//    &redirect_uri=YOUR_CALLBACK_URL
+//    &response_type=code
+
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Server running at:', server.info.uri);
 });
